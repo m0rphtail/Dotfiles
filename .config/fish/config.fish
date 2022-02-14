278db853d5b96292f2fc80f4da35e37ce6a5a198
MorphTail <chitniskshitij@gmail.com>
2022-02-14 23:56:14 +0530

    push

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,19 +3,21 @@ if status is-interactive
 end
 set fish_greeting
 starship init fish | source
+zoxide init fish | source
 
-alias up="paru --skipreview -Syyu && flatpak update"
+alias up="paru --skipreview -Syyu"
 alias pkg="paru --skipreview"
-alias ls="lsd"
+alias ls="logo-ls"
 alias l='ls -l'
 alias la='ls -a'
 alias lla='ls -la'
 alias lt='ls --tree'
 alias rm="trash"
+alias vim="nvim"
+alias cd="z"
 
-fish_add_path /home/morphtail/Data/Development/GO/golib
+fish_add_path /home/morphtail/Development/GO/golib
 fish_add_path PATH=$PATH:$GOPATH/bin
-fish_add_path GOPATH=/home/morphtail/Data/Development/GO/code
+fish_add_path GOPATH=/home/morphtail/Development/GO/code
 
-#source /usr/share/nvm/init-nvm.sh
 fish_add_path PATH="$PATH:$HOME/.rvm/bin"
